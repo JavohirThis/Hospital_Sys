c83e764cedbdf3572cc46e75fb6bdee06f9708cf
JavohirThis <javohir.this@gmail.com>
2023-07-17 17:36:02 -0400

    modified:   config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -40,7 +40,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('admin/', admin.site.urls),
     path('v1/api/hospital/',include('hospital.urls')),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
